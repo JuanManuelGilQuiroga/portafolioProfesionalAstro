4107272db8a83ce3be1813bd25bb5aa4e8a01bc9
JuanManuelGilQuiroga <157772473+JuanManuelGilQuiroga@users.noreply.github.com>
2024-09-01 20:57:20 -0500

    feat: :tada: Starting portfolio repository with astro

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./src/components/**/*.{astro,css,html}",
+    "./src/layouts/**/*.{astro,css,html}",
+    "./src/pages/**/*.{astro,css,html}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
